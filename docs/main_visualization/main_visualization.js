29459d097f92decf2858141099f95d471f3d0bd1
M3lvinzz <melvinsasylum@gmail.com>
2025-05-14 16:54:41 -0700

    added interactivity

--- docs/main_visualization/main_visualization.js
+++ docs/main_visualization/main_visualization.js
@@ -20,80 +20,97 @@ function renderLineGraph(data) {
 
   // Create SVG
   const svg = d3
-  .select('#vis')
-  .append('svg')
-  .attr('viewBox', `0 0 ${width} ${height}`)
-  .style('overflow', 'visible');
+    .select("#vis")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .style("overflow", "visible");
 
   // Scales
   const xScale = d3
-  .scaleLinear()
-  .domain(d3.extent(data, (d) => d.min))
-  .range([0, width])
-  .nice();
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.min))
+    .range([0, width])
+    .nice();
 
   const yScale = d3
-  .scaleLinear()
-  .domain(d3.extent(data, (d) => d.temp))
-  .range([0, width])
-  .nice();
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.temp))
+    .range([0, width])
+    .nice();
 
   // Setting display window
   const margin = { top: 10, right: 10, bottom: 30, left: 20 };
   const usableArea = {
-  top: margin.top,
-  right: width - margin.right,
-  bottom: height - margin.bottom,
-  left: margin.left,
-  width: width - margin.left - margin.right,
-  height: height - margin.top - margin.bottom,
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
   };
 
   xScale.range([usableArea.left, usableArea.right]);
   yScale.range([usableArea.bottom, usableArea.top]);
-  
+
   // Gridlines
   const gridlines = svg
-  .append('g')
-  .attr('class', 'gridlines')
-  .attr('transform', `translate(${usableArea.left}, 0)`);
-  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+    .append("g")
+    .attr("class", "gridlines")
+    .attr("transform", `translate(${usableArea.left}, 0)`);
+  gridlines.call(
+    d3.axisLeft(yScale).tickFormat("").tickSize(-usableArea.width)
+  );
 
   // Set axes
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
-  
+
   svg
-  .append('g')
-  .attr('transform', `translate(0, ${usableArea.bottom})`)
-  .call(xAxis);
-  
+    .append("g")
+    .attr("transform", `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
   svg
-  .append('g')
-  .attr('transform', `translate(${usableArea.left}, 0)`)
-  .call(yAxis);
+    .append("g")
+    .attr("transform", `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
 
   // Creating the lines
   const micedata = [
-    data.filter(({ sex }) => sex === 'female'),
-    data.filter(({ sex }) => sex === 'male')
-  ]
-  
-  const line = d3.line()
-  .x(d => xScale(d.min))
-  .y(d => yScale(d.temp));
-  const colors = d3.scaleOrdinal(d3.schemeCategory10)
+    data.filter(({ sex }) => sex === "female"),
+    data.filter(({ sex }) => sex === "male"),
+  ];
+
+  const line = d3
+    .line()
+    .x((d) => xScale(d.min))
+    .y((d) => yScale(d.temp));
+  const colors = d3.scaleOrdinal(d3.schemeCategory10);
 
   const path = svg
     .append("g")
-    .selectAll('path')
+    .selectAll("path")
     .data(micedata)
-    .join('path')
-    .attr('class', 'stock-lines')
-    .attr('d', line)
-    .style('stroke', (d, i) => colors(d[i].sex))
-    .style('stroke-width', 2)
-    .style('fill', 'transparent');
+    .join("path")
+    .attr("class", "stock-lines")
+    .attr("d", line)
+    .style("stroke", (d, i) => colors(d[i].sex))
+    .style("stroke-width", 2)
+    .style("fill", "transparent");
+
+  //Interactivity
+  const tooltipElements = i.createTooltip(svg);
+
+  i.addTooltipInteraction(
+    svg,
+    data,
+    micedata,
+    xScale,
+    yScale,
+    usableArea,
+    tooltipElements,
+    colors
+  );
 }
 
 const mice = await loadData("dataset/avgsMin.json");
