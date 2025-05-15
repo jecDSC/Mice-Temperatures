1c3a7cf3d39de4de1e774246851573d63c947211
M3lvinzz <melvinsasylum@gmail.com>
2025-05-14 19:50:14 -0700

    sub_vis"

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import renderLineGraph from "./main_visualization/main_visualization.js";
-
+import renderLineGraph_sub from "./main_visualization/sub_visualization.js";
 // Load Data
 async function loadData(path) {
   const data = await d3.csv(path);
@@ -12,3 +12,5 @@ const mice = await loadData("./dataset/average_temp.csv");
 renderLineGraph(mice);
 
 //Sub visualization
+const diff = await loadData("./dataset/male_female_difference.csv");
+renderLineGraph_sub(diff);
